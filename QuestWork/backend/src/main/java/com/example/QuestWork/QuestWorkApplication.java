e7b196afcceb3beb0aacd7f45926400261734962
Han <ehdrkfnwn1@naver.com>
2026-04-10 15:26:05 +0900

    first commit

---
+++ QuestWork/backend/src/main/java/com/example/QuestWork/QuestWorkApplication.java
@@ -0,0 +1,13 @@
+package com.example.QuestWork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestWorkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuestWorkApplication.class, args);
+	}
+
+}
